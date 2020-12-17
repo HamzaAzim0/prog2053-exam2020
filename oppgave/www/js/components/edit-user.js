f60db0c2ee64c2dadcb680c38cadc588e152fba2
Your Name <you@example.com>
2020-12-17 11:48:34 +0100

    slight changes

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -13,24 +13,24 @@ class EditUser extends LitElement {
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     </head>
     <form onsubmit="javascript: return false;" id="userForm" method="POST">
-    <div class="form-group pt-3 ml-5" style="width: 20rem;">
+    <div class="form-group pt-3 ml-5" style="width: 25rem;">
       <label for="email">Email:</label>
       <input class="form-control" id="uname" name="uname" type="text" value="${this.user.uName}" required>
       <input type="hidden" id="uid" name="uid" value="${this.user.uid}">
     </div>
-    <div class="form-group pt-1 ml-5" style="width: 20rem;">
+    <div class="form-group pt-1 ml-5" style="width: 25rem;">
       <label for="firstName">First Name:</label>
       <input class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
     </div>
-    <div class="form-group pt-1 ml-5" style="width: 20rem;">
+    <div class="form-group pt-1 ml-5" style="width: 25rem;">
       <label for="lastName">Last Name:</label>
       <input class="form-control" id="lastName" name="lastName" type="text" value="${this.user.lastName}" required>
     </div>
-    <div class="form-group pt-1 ml-5" style="width: 20rem;">
+    <div class="form-group pt-1 ml-5" style="width: 25rem;">
       <label for="oldpwd">Old Password:</label>
       <input type="password" class="form-control" id="oldpwd" name="oldpwd" type="text" value="">
     </div>
-    <div class="form-group pt-1 ml-5" style="width: 20rem;">
+    <div class="form-group pt-1 ml-5" style="width: 25rem;">
       <label for="newpwd">New Password:</label>
       <input type="password" class="form-control" id="pwd" name="pwd" type="text" value="">
   </div>
